6bd54df0fc8e7bbb3cfb33b4eb8318dcbc8ebc1d
Martin Poirier Theoret <mpoiriert@gmail.com>
2016-01-01 23:26:23 -0500

    end function to return the json helper

--- Helper/JsonRequestPropertyHelper.php
+++ Helper/JsonRequestPropertyHelper.php
@@ -1,14 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mpoirier
- * Date: 01/01/16
- * Time: 3:15 PM
- */
 
 namespace Draw\Bundle\DrawTestHelperBundle\Helper;
 
-
-class JsonRequestPropertyHelper {
-
+class JsonRequestPropertyHelper extends PropertyHelper
+{
+    /**
+     * @return JsonHelper
+     */
+    public function end()
+    {
+        return $this->requestHelper->jsonHelper();
+    }
 }
